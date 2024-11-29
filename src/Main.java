6ecc22bb3e1e5997cb56304ebfd1dc2a1a1840d3
Aleksandr Chernov <chernovaleksander81@yandex.ru>
2024-11-29 15:28:31 +0300

    changes commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int balance = 250; // начальный баланс
+        int bonusScale = 100; // шкала начисления бонусов
+        int replenishment = 1550; // сумма пополнения
+        int bonus;
+
+        if (replenishment >= 1000){
+            bonus = replenishment / bonusScale;
+        } else {
+            bonus = 0;
+        }
+
+        int totalBalance = balance + replenishment + bonus;
+
+        System.out.println("Вы пополнили счёт на " + replenishment + " руб.");
+        System.out.println("Ваш бонус составил " + bonus + " руб.");
+        System.out.println("Ваш баланс " + totalBalance + " руб.");
     }
 }
